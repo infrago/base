8caea6a637040149285b9fe145a419ec29d476ed
cheearsan <cheearsan@gmail.com>
2026-03-03 19:45:50 -0800

    update

---
+++ http.go
@@ -0,0 +1,13 @@
+package base
+
+const (
+	GET     = "get"
+	POST    = "post"
+	PUT     = "put"
+	DELETE  = "delete"
+	PATCH   = "patch"
+	OPTIONS = "options"
+	HEAD    = "head"
+	CONNECT = "connect"
+	TRACE   = "trace"
+)
